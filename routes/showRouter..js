cc56eb126d5b6e7f19ce2b30436cc539f6218dde
tanvir <shovontanvir124@gmail.com>
2023-06-25 17:26:36 +0600

    assigned creator role for add a show

--- routes/showRouter..js
+++ routes/showRouter..js
@@ -7,7 +7,7 @@ const {
 const { addCastAndCrews } = require("../middleware/addCastAndCrews");
 const { adminAuth } = require("../middleware/auth");
 
-router.post("/add",adminAuth(["admin"]), addCastAndCrews, addShow);
+router.post("/add",adminAuth(["admin","creator"]), addCastAndCrews, addShow);
 router.get("/all", findAllShow);
 router.get("/findOne", findShowById);
 
